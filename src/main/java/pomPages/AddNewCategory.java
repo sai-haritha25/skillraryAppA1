e34bde0b0bfe9b5ef5dd3c65ec0eed86306435a4
saiharitha <149862947+sai-haritha25@users.noreply.github.com>
2023-11-09 11:27:37 +0530

    Update AddNewCategory.java

--- src/main/java/pomPages/AddNewCategory.java
+++ src/main/java/pomPages/AddNewCategory.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+//this is addnewcategory
 public class AddNewCategory {
 	
 	@FindBy(xpath = "//b[text()='Add New Category']")
